799af4f95bb8529069612892c82ccceeb852d777
Shubhbhatia <shubhbhatia27@gmail.com>
2017-08-19 14:19:29 -0400

    init

---
+++ Assets/2D Platformer Controller/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    int points;
+    public Text scoreText;
+    int totalBananasAtStart = 0;
+    public GameObject Bananas;
+
+
+	// Use this for initialization
+	void Start () {
+
+        points = 0;
+        totalBananasAtStart = Bananas.transform.childCount;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int bananasLeft = Bananas.transform.childCount;
+        int bananaBonus = totalBananasAtStart - bananasLeft;
+
+        points = bananaBonus * 10;  //Maybe change this....
+
+        scoreText.text = "Score " + points;
+        
+	}
+
+    public void IncreaseScore(int p)
+    {
+        points += p;
+    }
+}
---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour {
+
+public void NewGameButton(string NewGameLevel)
+    {
+        SceneManager.LoadScene(NewGameLevel);
+
+    }
+
+	}
+
